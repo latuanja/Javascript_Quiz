b568bdebe993e93c166248b5ecc860166de8a7be
Music Box <latuanja.c@gmail.com>
2020-11-30 17:13:01 -0600

    Upload of questions template & first question and basic index layout

---
+++ assets/questions.js
@@ -0,0 +1,38 @@
+var jsQuestions = [
+	{
+	title: "What does JS stand for?",
+	choices: ["Jumbo Strings", "Jamaican Script", "JavaScript"],
+	answer: "JavaScript"
+},
+{
+	title: "",
+	choices: [],
+	answer: ""
+},
+{
+	title: "",
+	choices: [],
+	answer: ""
+},
+{
+	title: "",
+	choices: [],
+	answer: ""
+},
+{
+	title: "",
+	choices: [],
+	answer: ""
+},
+{
+	title: "",
+	choices: [],
+	answer: ""
+},
+{
+	title: "",
+	choices: [],
+	answer: ""
+},
+
+]
--- questions.js
+++
@@ -1,2 +0,0 @@
-//Declare variables for questions. Has to be equal to an array of objects
-//Setup: Question, choice, answer (as long as it's not same as variable)
